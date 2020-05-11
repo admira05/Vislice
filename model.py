f6c62ee100ac3dba4ee350ebbdd16306d1c84f71
Admira Abdic <admira.abdic@hotmail.com>
2020-05-11 19:20:08 +0200

    Dodatne metode

--- model.py
+++ model.py
@@ -29,7 +29,64 @@ class Igra:
     def pravilne_crke(self):
         return [c for c in self.crke if c in self.geslo]    
 
+    def stevio_napak(self):
+        return len(self.napacne_crke())
+
+    def poraz(self):
+        return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK
     
+    def zmaga(self):
+        for c in self.geslo:
+            if c not in self.crke:
+                return False
+        return True
+
+     #all([a, b, c])   preveri ali so vsi true
+    #to pomen a and b and c
+    #return all(c in self.crka for c in self.geslo)
 
     
+    def nepravilni_ugibi(self):
+        return " ".join(self.napacne_crke())
+    
+    def pravilni_del_gesla(self):
+        trenutno = ""
+        for crka in self.geslo:
+            if crke in self.crke:
+                trenutno += crka
+            else:
+                trenutno += "_"
+        return trenutno
+
+
+    def ugibaj(self, ugibana_crka):
+        ugibana_crka = ugibana_crka.lower()
+
+        if ugibana_crka in self.crke:
+            return PONOVLJENA_CRKA
+
+        self.crke.append(ugibana_crka)
+        
+        if ugibana_crka in self.geslo:
+            #kle je uganil, preverjamo ce je zmagu
+            if selg.zmaga()
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        else: 
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
+
+
+def nova_igra():
+    nakljucna_beseda = random.choice(bazen_besed)
+    return Igra(nakljucna_beseda)
+        
+
+
+
+
+
 
